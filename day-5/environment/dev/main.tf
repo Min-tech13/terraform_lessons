70c1c63db6d50882d919550c380d275d17b4aeb0
Mitya00 <kurbanaliev.mintemir@gmail.com>
2023-02-01 08:59:46 +0600

    this is for day 5

---
+++ day-5/environment/dev/main.tf
@@ -0,0 +1,35 @@
+#----------------------------------------------------------
+# My Terraform
+#
+# Use Our Terraform Module to create AWS VPC Networks
+#
+# Made by Denis Astahov. Summer 2019
+#----------------------------------------------------------
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc-default" {
+  source = "../../modules/networking"
+  
+}
+
+
+module "autoscaling-default" {
+  source = "../../modules/autoscaling"
+  vpc_id     =  module.vpc-default.vpc_id
+  subnet_ids  =  module.vpc-default.public_subnet_ids
+  
+}
+
+#====================================================
+output "vpc_id" {
+  value = module.vpc-default.vpc_id
+
+}
+
+output "public_subnet_ids" {
+  value = module.vpc-default.public_subnet_ids
+}
+
+
---
+++ day-5/environment/dev/veriables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+  description = "AWS Region where to provision VPC Network"
+  default     = "us-east-1"
+}
